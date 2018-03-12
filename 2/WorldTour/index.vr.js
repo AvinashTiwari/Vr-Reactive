474f5627386823acbfb9de8b5b2fab6da53d2232
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-11 18:26:00 -0600

    Modifed world tour

--- 2/WorldTour/index.vr.js
+++ 2/WorldTour/index.vr.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 import {View , Text, Pano, AppRegistry, asset, StyleSheet} from 'react-vr';
 
+
+const places = [{
+   title: 'Island Paradise'
+},
+ {
+   title: 'Starry Nights'
+
+ },
+ {
+   title: 'Winter Outdoors'
+
+ },
+ {
+   title: 'Light Shows'
+
+ }
+]
+
 class WorldTour extends Component{
 constructor()
 {
@@ -21,27 +39,72 @@ render() {
      <View
      style={styles.menuButton}
      onEnter = {() => this.toggleMenu()}>
-      <Text style={styles.menuText}>{this.state.showMenu ? 'Close Menu' : 'Open Menu'}</Text>
+      <Text style={styles.menuButtonText}>{this.state.showMenu ? 'Close Menu' : 'Open Menu'}</Text>
      </View>
+      {
+        this.state.showMenu ?
+        <View style={styles.menu}>
+          {
+            places.map((place,index) => {
+              return (
+                <View
+style={styles.menuItem}
+                key={index}>
+                <Text
+                style={styles.menuItemText}
+                >{place.title }</Text>
+                </View>
+              )
+            })
+          }
+        </View>
+        :
+          <View></View>
+      }
     </View>
   )
 }
 };
 
 const styles = StyleSheet.create({
+  menu: {
+    width: 5,
+    height: 1.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-around',
+transform:[{ translate: [-2, 0 ,-7.5]}]
+  },
   menuButton: {
     backgroundColor: '#fff',
     borderRadius: 0.25,
     width: 0.5,
+    height: 0.5,
     alignItems: 'center',
     justifyContent:'center',
     borderWidth:  0.01,
-    transform:[{ translate: [-2, 0 ,-5]}]
+    transform:[{ translate: [-4, 0.5 ,-4]}]
 
   },
-  menuText: {
+  menuButtonText: {
     textAlign: 'center',
     fontSize: 0.15,
+    color: '#000'
+
+  },
+    menuItem: {
+    alignItems: 'center',
+    justifyContent:'center',
+    width: 1,
+    height: 1,
+    borderRadius: 0.5,
+    borderWidth:  0.02,
+    backgroundColor:'#ffff'
+
+  },
+  menuItemText:{
+    textAlign: 'center',
+    fontSize: 0.2,
     color: '#000'
   }
 });
