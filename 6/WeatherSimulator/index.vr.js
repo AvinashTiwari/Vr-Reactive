6cb16b77ceec26371ef3a5b571c86961143b6919
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-24 14:35:55 -0600

    react

--- 6/WeatherSimulator/index.vr.js
+++ 6/WeatherSimulator/index.vr.js
@@ -7,7 +7,24 @@ import {
   View,
 } from 'react-vr';
 
+const apikey ='165c1e5a8c00a7d9586120f7f9a91b41';
+
 class WeatherSimulator extends Component {
+  constructor(){
+    super();
+    this.state ={
+      weatherObject: {}
+    }
+  }
+
+compontentDidMount() {
+  console.log('Inside compontentDidMount' );
+
+  fetch('http://api.openweathermap.or/data/2.5/weather?q=SanFrancisco&appid=165c1e5a8c00a7d9586120f7f9a91b41', {
+      method: 'GET'
+  }).then (response => response.json()).then(json => this.setState({weatherObject: json}) );
+}
+
   render() {
     return (
       <View>
